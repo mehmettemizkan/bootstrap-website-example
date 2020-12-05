72e9d569e91c4599535fc1c6f5a84d750aee04f8
mehmettemizkan <mehmettemizkann@gmail.com>
2020-12-05 21:32:20 +0300

    Dosyalar olusturuldu.

---
+++ mtemizkan.php
@@ -0,0 +1,253 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title> Mehmet Temizkan</title>
+	<meta name="description" content="Bootstrap4 Örnek Sayfası">
+	<meta name="keywords" content="mehmet temizkan, bootstrap4 ornek sayfa">
+	<meta name="author" content="Mehmet Temizkan">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="shortcut icon" type="image/jpg" href="/me.jpg"/>
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+
+<body>
+
+	<nav class="navbar navbar-expand-sm navbar-dark bg-danger">
+		<div class="container">
+			<a href="#" class="navbar-brand">MEHMET TEMİZKAN</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#menue">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="menue">
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item ">
+						<a href="#" class="nav-link active"><i class="fa fa-home" aria-hidden="true"></i> ANASAYFA</a>
+					</li>
+					<li class="nav-item">
+						<a href="#" class="nav-link">MATEMATİK</a>
+					</li>
+					<li class="nav-item">
+						<a href="#" class="nav-link">BİLGİSAYAR</a>
+					</li>
+					<li class="nav-item">
+						<a href="#" class="nav-link"> HAKKIMDA <i class="fa fa-address-card"></i></a>
+					</li>
+				</ul>
+			</div>			
+		</div>
+	</nav>
+
+	<div class="container">
+    <div class="row mt-4">
+     
+      <div class="col-md-8">
+
+        <h1 style="text-align: center;" >Makaleler</h1>
+        <hr class="bg-success">
+
+        <div class="card mb-4">
+          <img class="card-img-top" src="http://via.placeholder.com/750x300">
+          <div class="card-body">
+          	<h2 class="card-title">Makale Başlığı</h2>
+            <!-- <h2 class="card-title"> <a href="#" target="_blank"> Örnek Bir Table Sayfası </a> </h2> -->
+
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+              quis nostrud exercitation ullamco <strong>lorem ipsum dolor</strong> laboris nisi ut aliquip ex ea commodo
+              consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+              cillum dolore eu fugiat nulla pariatur. <i>Excepteur</i> sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <a href="#" class="btn btn-success">Devamını Oku <i class="fa fa-angle-right" aria-hidden="true"></i>
+            </a>
+          </div>
+          
+          <div class="card-footer text-muted">
+            <span>Yayınlandığı Tarih: 01.01.2020 20:20</span>
+          </div>
+        </div>
+
+        <div class="card mb-4">
+          <img class="card-img-top" src="http://via.placeholder.com/750x300">
+          <div class="card-body">
+            <h2 class="card-title">Makale Başlığı</h2>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+              consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <a href="#" class="btn btn-success">Devamını Oku <i class="fa fa-angle-right" aria-hidden="true"></i>
+            </a>
+          </div>
+          <div class="card-footer text-muted">
+            <span>Yayınlandığı Tarih: 01.01.2020 20:20</span>
+          </div>
+        </div>
+
+        <div class="card mb-4">
+
+          <img class="card-img-top" src="http://via.placeholder.com/750x300">
+          <div class="card-body">
+            <h2 class="card-title">Makale Başlığı</h2>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+              consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <a href="#" class="btn btn-success">Devamını Oku <i class="fa fa-angle-right" aria-hidden="true"></i>
+            </a>
+          </div>
+
+          <div class="card-footer text-muted">
+            <span>Yayınlandığı Tarih: 01.01.2020 20:20</span>
+          </div>
+
+        </div>
+
+        <div class="card mb-4">
+
+          <img class="card-img-top" src="http://via.placeholder.com/750x300">
+          <div class="card-body">
+            <h2 class="card-title">Makale Başlığı</h2>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+              consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <a href="#" class="btn btn-success">Devamını Oku <i class="fa fa-angle-right" aria-hidden="true"></i>
+            </a>
+          </div>
+
+          <div class="card-footer text-muted">
+            <span>Yayınlandığı Tarih: 01.01.2020 20:20</span>
+          </div>
+
+        </div>       
+      </div>
+
+      
+      <div class="col-md-4">
+
+       <div class="card">
+        <h6 class="card-header bg-info text-white">Arama Kutusu</h6>
+        <div class="card-body">
+          <div class="input-group">
+            <input type="text" class="form-control" placeholder="Kelime Giriniz..." name="">
+            <button class="btn btn-secondary" type="button">Ara</button></span>
+          </div>
+        </div>
+      </div>
+
+      <div class="card my-4">
+        <h6 class="card-header bg-info text-white">Kategoriler</h6>
+        <div class="card-body">
+          <div class="row">
+            <div class="col-md-6">
+              <ul class="list-unstyled">
+                <li>
+                  <a href="#"><i class="fa fa-html5" aria-hidden="true"></i></i> HTML5</a>
+                </li>
+                <li>
+                  <a href="#"><i class="fa fa-css3" aria-hidden="true"></i>CSS3</a>
+                </li>
+                <li>
+                  <a href="#"><i class="fa fa-home" aria-hidden="true"></i>Bootstrap4</a>
+                </li>
+                <li>
+                  <a href="#"><i class="fa fa-home" aria-hidden="true"></i>PHP</a>
+                </li>
+                <li>
+                  <a href="#"><i class="fa fa-home" aria-hidden="true"></i>Java</a>
+                </li>
+                <li>
+                  <a href="#"><i class="fa fa-home" aria-hidden="true"></i>Ajax</a>
+                </li>
+              </ul>
+            </div>
+            <div class="col-md-6">
+              <ul class="list-unstyled">
+                <li>
+                  <a href="#"><i class="fa fa-code"></i></i>Matlab</a>
+                </li>
+                <li>
+                  <a href="#"><i class="fa fa-home" aria-hidden="true"></i>Maple</a>
+                </li>
+                <li>
+                  <a href="#"><i class="fa fa-home" aria-hidden="true"></i>Haskell</a>
+                </li>
+                <li>
+                  <a href="#"><i class="fa fa-home" aria-hidden="true"></i>R</a>
+                </li>
+                <li>
+                  <a href="#"><i class="fa fa-home" aria-hidden="true"></i>Gap</a>
+                </li>
+                <li>
+                  <a href="#"><i class="fa fa-home" aria-hidden="true"></i>GeoGebra</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="card my-4">
+        <h6 class="card-header bg-info text-white">Reklam Alanı</h6>
+        <div align="center" class="card-body">
+          <img class="img-fluid" src="http://via.placeholder.com/400x800" alt="Reklam Alanı">
+        </div>
+      </div>
+
+
+     
+
+
+      <footer>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-success fixed-bottom">
+          <div class="container">
+            
+            <a href="#" class="navbar-brand" data-toggle="collapse" data-target="#altmenuac">MAİLLİST KAYIT</a>
+            <div class="collapse navbar-collapse" id="altmenuac">
+
+            	<form class="form-inline">
+	                <input type="email" class="form-control col-auto mr-2" placeholder="Mail Adresi..." name="">
+	                <input type="text" class="form-control col-auto " placeholder="Ad ve Soyad..." name="">
+	                <button class="btn btn-secondary my-1 ml-2">Kayıt Ol!</button>                
+              	</form>
+
+              	<ul class="navbar-nav ml-auto">
+	                <li class="nav-item ">
+	                  <a href="#" class="nav-link">Gizlilik</a>
+	                </li>
+	                <li class="nav-item">
+	                  <a href="#" class="nav-link">Koşullar ve Şartlar</a>
+	                </li>
+              	</ul>
+
+            </div>
+
+          </div>
+
+        </nav>
+
+      </footer>
+
+    </div>
+    </div>
+
+	<!-- Optional JavaScript -->
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
